a1e734e90882520a5cd50d08af7725437ff2a4c8
Emily Kent <kentembowemily@gmail.com>
2024-05-05 13:49:52 +0100

    Created socials component and added it to contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Form from '../components/form/form'
+import Socials from '../components/socials/socials'
 
 function Contact(){
     return(
         <div>
             <Form></Form>
+            <Socials></Socials>
         </div>
     )
 }

5d9fcfa5015a203fc3bda96954c7b32ca9a7c80b
Emily Kent <kentembowemily@gmail.com>
2024-05-05 13:47:59 +0100

    Contact page created

---
+++ src/pages/Contact.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Form from '../components/form/form'
+
+function Contact(){
+    return(
+        <div>
+            <Form></Form>
+        </div>
+    )
+}
+
+export default Contact
